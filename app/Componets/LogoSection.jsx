75564e5f33b654f44ef86725d64f426e1e9c8708
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-01-25 17:26:14 -0500

    update!

--- app/Componets/LogoSection.jsx
+++ app/Componets/LogoSection.jsx
@@ -4,7 +4,7 @@ const LogoSection = () => {
     return (
         <div className='bg-gray-200 overflow-hidden h-[2rem]  relative'>
 
-            <video autoPlay loop playsInline muted className='h-auto opacity-25  m-auto object-cover  w-full' src="/videos/smoke.mp4"></video>
+            <video autoPlay loop playsInline muted className='h-auto opacity-50  m-auto object-cover  w-full' src="/videos/smoke.mp4"></video>
 
         </div>
     )
